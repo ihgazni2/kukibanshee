a0236befd19084632dc93dceab334364cb769cb6
ihgazni2 <286264978@qq.com>
2018-03-14 15:26:15 +0800

    remove old build

--- kukibanshee/nozdormu.py
+++ kukibanshee/nozdormu.py
@@ -103,7 +103,7 @@ TIMEFMT = {
     'rfc850_a':'%a, %d-%b-%y %H:%M:%S GMT',
     'asctime':'%a, %b %d %H:%M:%S %Y',
     '%a, %d %b %Y %H:%M:%S GMT':'rfc1123',
-    '%a, %d %b %Y %H:%M:%S':'rfc1123_notz'
+    '%a, %d %b %Y %H:%M:%S':'rfc1123_notz',
     '%a, %d %b %Y %H:%M:%S %z':'rfc1123_tzoffset',
     '%a, %d-%b-%Y %H:%M:%S GMT':'rfc1123_hypen',
     '%A, %d-%b-%y %H:%M:%S GMT':'rfc850',
