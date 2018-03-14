4fe788f3f6f5f3e29cf50974b549fd1bf86ad537
ihgazni2 <286264978@qq.com>
2018-03-14 17:27:17 +0800

    remove old build

--- kukibanshee/araq.py
+++ kukibanshee/araq.py
@@ -1,5 +1,53 @@
 import re
 
+#copied from xdict.utils
+def str_lstrip(s,char,count):
+    '''
+        str_lstrip('sssa','s',0)
+        str_lstrip('sssa','s',1)
+        str_lstrip('sssa','s',2)
+        str_lstrip('sssa','s',3)
+        str_lstrip('sssa','s',4)
+    '''
+    c = 0
+    for i in range(0,s.__len__()):
+        if(c==count):
+            break
+        else:
+            if(s[i] == char):
+                c = c+1
+            else:
+                break
+    if(c==0):
+        return(s)
+    else:
+        return(s[c:])
+
+def str_rstrip(s,char,count):
+    '''
+        str_rstrip('asss','s',0)
+        str_rstrip('asss','s',1)
+        str_rstrip('asss','s',2)
+        str_rstrip('asss','s',3)
+        str_rstrip('asss','s',4)
+    '''
+    c = 0
+    for i in range(s.__len__()-1,-1,-1):
+        if(c==count):
+            break
+        else:
+            if(s[i] == char):
+                c = c+1
+            else:
+                break
+    if(c==0):
+        return(s)
+    else:
+        ei = s.__len__() - c
+        return(s[:ei])
+
+
+
 #regex 
 def _real_dollar(s,m):
     '''
